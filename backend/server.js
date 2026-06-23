c4adc9c7d2525b03906a84babeda97d8c1d69522
MFRRayhan <md.fazlerabbirayhan786@gmail.com>
2026-06-23 08:17:57 +0600

    feat: add MongoDB connection setup and update dependencies

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import products from "./data/products.js";
 import cors from "cors";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+connectDB();
 
 const port = process.env.PORT || 4000;
 const app = express();
