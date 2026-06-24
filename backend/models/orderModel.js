101a771ddb6eefc3741e3edda755d7372c624aac
MFRRayhan <md.fazlerabbirayhan786@gmail.com>
2026-06-24 08:10:07 +0600

    fix: correct typo in orderItems and enhance order schema with shipping and payment details

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import product from "./productModel";
 
 const orderSchema = new mongoose.Schema(
   {
@@ -9,7 +8,7 @@ const orderSchema = new mongoose.Schema(
       ref: "User",
     },
 
-    oderItems: [
+    orderItems: [
       {
         name: {
           type: String,
@@ -35,6 +34,80 @@ const orderSchema = new mongoose.Schema(
         },
       },
     ],
+
+    shippingAddress: {
+      address: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      postalCode: {
+        type: String,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+      },
+    },
+
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+
+    paymentResult: {
+      id: { type: String },
+      status: { type: String },
+      updateTime: { type: String },
+      emailAddress: { type: String },
+    },
+
+    itemsPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+
+    taxPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+
+    shippingPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+
+    paidAt: {
+      type: Date,
+    },
+
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
+    },
   },
   { timestamps: true },
 );
