49729947bc54b9a4d56ade2c31c53833976a1611
MFRRayhan <md.fazlerabbirayhan786@gmail.com>
2026-06-28 12:15:44 +0600

    feat: integrate Redux Toolkit for state management, add API slices and constants

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./routes/router.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { router } from "./routes/router.jsx";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </Provider>
   </StrictMode>,
 );
